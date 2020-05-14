9302094c1f44beaf096c3807de999d169ae02780
shifat16solaiman@gmail.com <solaimanshifat3@gmail.com>
2020-05-14 22:08:22 +0600

    Tag Added For Collision

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 50f;
+    [SerializeField] float mainThrust = 5f;
+
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "friendly":
+                // do nothing
+                print("OK"); //todo remove
+                break;
+            case "Fuel":
+                print("Fuel"); //todo remove
+                break;
+            default:
+                print("Dead");
+                // kill player
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+    private void Rotate() {
+
+        rigidbody.freezeRotation = true; //take manual control
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false;
+    }
+
+}
